0ea935409e70516768e4ee05865ff3df624f95e7
Maikel <maikel.souza@gmail.com>
2018-01-08 21:41:57 -0200

    Continuação

--- src/br/com/condominiosvirtuais/thread/EmailThread.java
+++ src/br/com/condominiosvirtuais/thread/EmailThread.java
@@ -47,7 +47,7 @@ public class EmailThread extends Thread {
 				List<Email> listaEmail = this.emailService.buscar(Integer.parseInt(this.configuracaoAplicacaoService.getConfiguracoes().
 			    get(ConfiguracaoAplicacaoEnum.QTD_ENVIO_EMAIL_LOTE.getChave())));
 				for (Email email : listaEmail) {
-					this.emailService.enviarHTML(email, principalMB.getCaminhoAplicacao(), principalMB.getCharset());
+					//this.emailService.enviarHTML(email, principalMB.getCaminhoAplicacao(), principalMB.getCharset());
 					this.emailService.excluir(email);					 
 				 }
 				logger.info("Fim do processo para envio de email em : " + new Date());
